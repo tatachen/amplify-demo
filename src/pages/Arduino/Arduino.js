7f41a30a445669e62435dadbb80dcbdf23d7dbcb
ELAN\10044 <tata.chen@emc.com.tw>
2024-04-03 14:00:47 +0800

    update

---
+++ src/pages/Arduino/Arduino.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react';
+
+const Arduino = () => {
+    return (
+        <Segment>
+        </Segment>
+    )
+}
+
+export default Arduino;
